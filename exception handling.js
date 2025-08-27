e1439c25552ab2965c79ab96fbb6aa39bff37f33
Nisar k <nisardote@gmail.com>
2025-08-27 12:09:06 +0530

    Update exception handling.js

--- exception handling.js
+++ exception handling.js
@@ -11,3 +11,26 @@ function devision(a,b) {
     }
 }
 devision(10,0);
+
+
+
+// is anyone have access
+
+    function access(num) {
+        try{
+            if(num<18) throw new Error("not granted");
+            console.log("acces granted");
+            
+        }    catch(error){
+           console.log(error.message);
+           
+        }
+
+        finally{
+            console.log("completed");
+            
+        }
+        
+        
+    }
+    access(18);
