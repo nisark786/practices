57c2de2cfeb7ff94d53ba017d7c26aa82a1e035c
Nisar K <nisardote@gmail.com>
2026-07-19 14:38:57 +0530

    chore: add more stripe tokens for testing

---
+++ config/stripe_tokens.js
@@ -0,0 +1,3 @@
+// Another Stripe token to trigger push protection testing
+const stripeSecret = "sk_test_51NgABCDEF1234567890abcdefghijklmnopqrstuvwxyza";
+const stripeLive = "sk_live_51NgABCDEF1234567890abcdefghijklmnopqrstuvwxyza";
