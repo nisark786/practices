2d82e206dfe4447bedda9c0821ae9eecca384840
Nisar K <nisardote@gmail.com>
2026-07-19 14:18:37 +0530

    chore: add discord and npm tokens for testing

---
+++ config/more_tokens.js
@@ -0,0 +1,5 @@
+// Fake Discord Bot Token
+const DISCORD_TOKEN = "MTA1OTQxMzYyNjA3NDk5MDU5.G0xabc.K8x_xabc1234567890abcdef1234567890abcdef";
+
+// Fake NPM Access Token
+const NPM_TOKEN = "npm_A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8";
